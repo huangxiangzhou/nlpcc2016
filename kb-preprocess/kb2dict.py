cc43b933bf8b0731d805f4971194543750f80444
huangxiangzhou <huangxiangzhou@zju.edu.cn>
2017-02-20 09:45:35 +0100

    import kb as python dict

---
+++ kb-preprocess/kb2dict.py
@@ -0,0 +1,36 @@
+import sys
+import codecs
+
+encode = 'utf8'
+
+if len(sys.argv) >= 4:
+    encode = sys.argv[3]
+
+fi = open(sys.argv[1], 'r', encoding=encode)
+fo = open(sys.argv[2], 'w', encoding='utf8')
+
+
+kbDict={}
+newEntityDic={}
+i = 0
+for line in fi:
+    i += 1
+    print('exporting the ' + str(i) + ' triple', end='\r', flush=True)
+    entityStr = line[:line.index(' |||')]
+    tmp = line[line.index('||| ') + 4:]
+    relationStr = tmp[:tmp.index(' |||')]
+    objectStr = tmp[tmp.index('||| ') + 4:].strip('\n')
+    if entityStr not in kbDict:
+        newEntityDic = {relationStr:objectStr}
+        kbDict[entityStr] = newEntityDic
+    else:
+        kbDict[entityStr][relationStr] = objectStr
+
+
+fo.write(str(kbDict))
+
+fi.close()
+fo.close()
+    
+
+    
