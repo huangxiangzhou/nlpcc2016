8cbceb4d20b0a65f07fbfa19bec238a02c0af73b
huangxiangzhou <huangxiangzhou@zju.edu.cn>
2017-03-18 14:43:04 +0800

    Add C-extension for calculate cosine similarity.

--- python-lcs/lcs.c
+++ python-lcs/lcs.c
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include <math.h>
 
 wchar_t *lcommon(const wchar_t *string1, const wchar_t *string2) {
 	int strlen1 = wcslen(string1);
@@ -52,10 +53,77 @@ static PyObject * lcs_function(PyObject *self, PyObject *args) {
     return ret; 
 }
 
+
+static PyObject * cosine_function(PyObject *self, PyObject *args) {
+    PyObject * listObj1; /* the list as vector1 */
+    PyObject * listObj2; /* the list as vector2 */
+    PyObject * doubleObj;  /* one double float in the list */
+    int vectorSize1 = 0;
+    int vectorSize2 = 0;
+    int i;
+    double *vector1 = NULL;
+    double *vector2 = NULL;
+    double dot = 0, norm1 = 0, norm2 = 0, cosineDistance=0;
+    
+    PyObject *ret = NULL;
+    /* the O! parses for a Python object (listObj) checked
+        to be of type PyList_Type */
+    if (! PyArg_ParseTuple( args, "O!O!", &PyList_Type, &listObj1, &PyList_Type, &listObj2)) 
+        return NULL;
+    
+    vectorSize1 = PyList_Size(listObj1);
+    vectorSize2 = PyList_Size(listObj2);
+    if (vectorSize1 <= 0 || vectorSize2 <= 0){
+        printf("The size of the two vectors must not be 0!");
+        return 0;
+    }
+    
+    if (vectorSize1 != vectorSize2){
+        printf("The size of the two vectors must be same!");
+        return 0;
+    }
+       
+    //printf("input size1 is %d\n", vectorSize1);
+    //printf("input size2 is %d\n", vectorSize2);
+    
+    vector1 = (double *)calloc(vectorSize1, sizeof(double));
+    vector2 = (double *)calloc(vectorSize2, sizeof(double));
+    
+    for (i = 0; i < vectorSize1; i++){
+        doubleObj = PyList_GetItem(listObj1, i); 
+        vector1[i] = PyFloat_AsDouble(doubleObj); 
+        doubleObj = PyList_GetItem(listObj2, i);         
+        vector2[i] = PyFloat_AsDouble(doubleObj);
+    }
+    
+    for (i = 0; i < vectorSize1; i++){
+        dot += vector1[i] * vector2[i];
+        norm1 += vector1[i] * vector1[i];
+        norm2 += vector2[i] * vector2[i];   
+    }
+    
+    cosineDistance = dot / (sqrt(norm1) * sqrt(norm2));
+    
+    
+    free(vector1);
+    free(vector2);
+    
+    
+    ret = Py_BuildValue("d", cosineDistance);
+
+    return ret; 
+}
+
 static PyMethodDef lcsMethods[] = {
     {
          "lcs",
          lcs_function,
+         METH_VARARGS,
+         ""
+    },
+    {
+         "cosine",
+         cosine_function,
          METH_VARARGS,
          ""
     },
